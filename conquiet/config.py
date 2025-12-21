97f341cf38249d917fc5fbe31d849e34bcfe31de
oded neaman <oded.neaman@coti.io>
2025-12-21 17:33:57 +0200

    refactor: remove unused DbConfig class to streamline configuration structure

--- conquiet/config.py
+++ conquiet/config.py
@@ -2,13 +2,6 @@ from dataclasses import dataclass
 
 
 @dataclass
-class DbConfig:
-    table_name: str
-    id_column: str = "id"
-    # Extra flags can be added later.
-
-
-@dataclass
 class QueueConfig:
     stream_key: str
     consumer_group: str
